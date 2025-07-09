9037cd8673953cd1e3c621d118d88525be842587
BeforeTheNightOwl <josiasmrico04@gmail.com>
2025-07-09 17:27:36 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,15 +1,18 @@
-# Criar um banco onde posso: depositar, sacar e ver o extrato, com a opção de sair.
+# Criar um banco onde posso: deposito, sacar e ver o extrato, com a opção de sair.
 # Máximo de vezes que posso sacar em um dia: 3
 # Máximo de dinheiro que posso sacar por vez: R$ 500
 # Formato moeda: R$ xxxx.xx
 
 LIMITE_SAQUE = 3
+MAXIMO_SAQUE = 500
+MINIMO_SAQUE = 0
+
 saque = 0
 quantidade_saques=0
 saldo = 0
-depositar = 0
-extrato = ""
+deposito = 0
 
+extrato = ""
 escolha = ""
 
 while escolha != "q":
@@ -23,21 +26,21 @@ while escolha != "q":
 
 
     #QUITAR
-    if escolha == "q":
+    if escolha.lower() == "q":
         break
 
 
     # SACAR
-    if escolha == "s":
+    elif escolha.lower() == "s":
 
         # Verifica se já sacou mais de 3 vezes e avisa caso for verdade
-        if quantidade_saques >= 3:
+        if quantidade_saques >= LIMITE_SAQUE:
             print("Você já sacou mais que 3 vezes, volte outro dia para sacar mais.")
         else:
             saque = float(input("Informar quantidade para sacar: "))
 
             # Verifica se o saque é maior que 500 reais ou menor igual que 0 e avisa caso for 
-            if saque > 500 or saque <= 0:
+            if saque > MAXIMO_SAQUE or saque <= MINIMO_SAQUE:
                 print("Deve sacar algum valor entre R$ 0 e R$ 500, nada fora desses parâmetros")
             else:
                 # Verifica se possui saldo suficiente para o saque e avisa se não tiver
@@ -49,20 +52,23 @@ while escolha != "q":
                     extrato += f"\n    saque: R$ {saque:.2f}"
 
 
-    #DEPOSITAR
-    if escolha == "d":
-        depositar = float(input("Digite o valor que deseje depositar: "))
-        if depositar < 0:
+    # DEPOSITAR
+    elif escolha.lower() == "d":
+        deposito = float(input("Digite o valor que deseje depositar: "))
+        if deposito < 0:
             print("Você deve depositar valores positivos, caso queira sacar deve presionar 's'.")
         else:
-            saldo += depositar
-            extrato +=f"\nDepositou: R$ {depositar:.2f}"
+            saldo += deposito
+            extrato +=f"\nDepositou: R$ {deposito:.2f}"
 
-    if escolha == "e":
+    # EXIBIR EXTRATO
+    elif escolha.lower() == "e":
         print(f'''
 ===================================
               EXTRATO
-    {extrato}
+    {extrato if extrato else "Não foi realizada nenhuma ação"}
 
-    Saldo: R$ {saldo}
+    Saldo: R$ {saldo:.2f}
 ''')
+    else:
+        print("sinal inválido. Tente novamente")
